bff18bf89c21a38dcb42bbf010bc6a8c1a1b5cd6
Benny <wonsang222@naver.com>
2025-02-24 21:20:39 +0900

    add CustomGraphView

---
+++ stockStalker/stockStalker/ChartView.swift
@@ -0,0 +1,15 @@
+//
+//  ChartView.swift
+//  stockStalker
+//
+//  Created by Wonsang Hwang on 2/24/25.
+//
+
+import UIKit
+
+class ChartView: UIView {
+    
+    override func draw(_ rect: CGRect) {
+        
+    }
+}
