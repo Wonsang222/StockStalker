f35f952b7661ed3414f8422e376d2f5b0d542a51
황원상 <wonsang222@naver.com>
2025-03-11 12:17:16 +0900

    custom control event

---
+++ stockStalker/stockStalker/Extensions/UISegmentController+.swift
@@ -0,0 +1,12 @@
+//
+//  UISegmentController.swift
+//  stockStalker
+//
+//  Created by Wonsang Hwang on 3/11/25.
+//
+
+import UIKit
+import RxCocoa
+import RxSwift
+
+extension Reactive where Base:
